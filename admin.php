8c7a04b01861dccdfe11453fcb6ed35c2e961c52
praseedm <prasi.praseed@gmail.com>
2016-04-20 23:14:26 +0530

    Added Admin login

---
+++ admin.php
@@ -0,0 +1,149 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>User Login</title>
+  
+  <link href="./raw/css" rel="stylesheet" type="text/css">    
+  <link rel="stylesheet" type="text/css" href="./raw/style.css">
+</head>
+
+<body>
+  <div id="main">
+ 
+	<div id="headerin">  
+	  
+	  <div id="welcome">
+	    <a href="index.html"><img src="raw/ktu.jpg" alt="logo" height="110" width="500"></a>
+	  </div><!--close welcome-->	  
+	  
+	  <div id="menubar_container">
+		
+		<div id="menubar">
+		
+          <div id="menu_items">
+	        <ul id="menu">
+              <li><a href="index.html">Home</a></li>
+              <li class="current"><a href="userlogin.php">User Login</a></li>
+              <li><a href="galery.html">Gallery</a></li>
+              <li><a href="contactus.html">Contact Us</a></li>
+            </ul>
+          </div><!--close menu-->
+		
+        </div><!--close menubar-->	
+		
+      </div><!--close menubar_container-->		 
+	
+	<div id="site_content">  
+	        
+	  <div id="content">		
+		
+		<div class="content_item">	
+			  	
+			<?php
+          		require_once('connect.php');
+          			session_start();
+					if(isset($_SESSION['dpt'])){
+						$user = $_SESSION['name'];
+						echo '<h1 style="text-align:center;">Welcome '.$user.'</h1>';
+						echo '<a href="logout.php"><h1 style="text-align:right;">Logout</h1></a>';
+						$dpt=$_SESSION['dpt'];
+						}
+						else{echo '<h4 style="text-align:center;color:red;"><br><br>Login Failed<br>Login to get acess</h4>';}
+          	?>		
+          		<div >
+          			<div>
+          				<h2 style="text-align:center;">ADD STUDENT</h2>
+          			<form method="POST" action="admin.php" >
+		 		 		<input type="text" placeholder="Studentname" name="name" required><br><br>
+						 <select name="dpt">
+						    <option value="cse">CSE</option>
+						    <option value="ce">CE</option>
+						    <option value="eee">EEE</option>
+						    <option value="mech">Mech</option>
+						  </select><br><br>
+		  				<input type="number" placeholder="phone no" name="phone" size ="10" value=""required><br><br>
+		  				<input type="submit" name='add_student' value="Add Student">
+		  			</form>
+		  				<?php 
+          	
+          					if(isset($_POST['add_student'])){
+          						if(empty($_POST['phone']) || strlen($_POST['phone'])!=10 )
+									{
+									  $data_missing[]='phone_no';
+									  echo '<h4 style="text-align:center;color:red;">wrong Phone number</h4>';
+									}else{$phone_no=trim($_POST['phone']);
+											$name=trim($_POST['name']);
+											$dpt=$_POST['dpt'];}
+								if(empty($data_missing)){
+									require_once('connect.php');
+									$query = "INSERT INTO students (name,dpt,id,phone)
+  								      VALUES('$name','$dpt',NULL,'$phone_no')";
+  								      $response = mysqli_query($dbc, $query);
+  								      if($response == 1){
+									   echo '<h4 style="text-align:center;color:red;">ADDED SUCESSFULLY</h4>';}
+									    mysqli_close($dbc); 
+									  }
+								}
+
+          				?>
+		  			</div>
+		  			<br><br>
+		  			<div>
+		  				<h2 style="text-align:center;">ADD FACULTY</h2>
+          			<form method="POST" action="admin.php" >
+		 		 		<input type="text" placeholder="Facultyname" name="name" required><br><br>
+		  				<select name="dpt">
+						    <option value="cse">CSE</option>
+						    <option value="ce">CE</option>
+						    <option value="eee">EEE</option>
+						    <option value="mech">Mech</option>
+						  </select><br><br>
+		  				<input type="text" placeholder="PassWord" name="password" required><br><br>
+		  				<input type="submit" name='add_faculty' value="Add Faculty">
+		  			</form>
+		  				<?php 
+          	
+          					if(isset($_POST['add_faculty'])){
+          						if(empty($_POST['password']) || strlen($_POST['password']) < 8 )
+									{
+									  $data_missing[]='password';
+									  echo '<h4 style="text-align:center;color:red;">password min 8 letters required</h4>';
+									}else{$password=trim($_POST['password']);
+											$name=trim($_POST['name']);
+											$dpt=$_POST['dpt'];}
+								if(empty($data_missing)){
+									require_once('connect.php');
+									$query = "INSERT INTO login (id,user,password,dpt)
+  								      VALUES(NULL,'$name','$password','$dpt')";
+  								      $response = mysqli_query($dbc, $query);
+  								      if($response == 1){
+									   echo '<h4 style="text-align:center;color:red;">ADDED SUCESSFULLY</h4>';}
+									    mysqli_close($dbc); 
+									  }
+								}
+
+          				?>
+		  			</div>
+          		</div>
+          	
+  			</br>
+  		</br> </br>
+  			<p > Welcome . Details of students and faculties Registered Under Department </p>
+  			
+	</div>	  							
+		
+		</div><!--close content_item-->	  
+	  
+	  </div><!--close content-->
+<div class="bottom_footer">
+  <p>&copy; Praseed Melethil</p>
+</div>
+	  
+	</div><!--close site_content--> 
+ 
+  <!--close main-->
+ 
+  
+  
+
+</body></html>
